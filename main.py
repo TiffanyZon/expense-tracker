b3ac9c84a5106754e05f103a6ecb5b2cbe6b5d1c
TiffanyZon <tiffanyz@hotmail.se>
2026-01-29 18:25:30 +0100

    Add second tab for overview of expenses

--- main.py
+++ main.py
@@ -2,37 +2,39 @@ import streamlit as st
 from datetime import date
 import pandas as pd
 import numpy as np
-import csv
 
-st.title("Expense Tracker")
-st.write("Register your expenses and get and overview of your spendings")
+tab1, tab2 = st.tabs(["Add expense", "Overview"])
 
-st.header("Add a new expense")
-with st.form(key="expense_form"):
-    amount = st.number_input("Amount (SEK)", min_value=0.0, format="%.2f")
-    category = st.selectbox(
-        "Category", ["Food", "Transport", "Housing", "Entertainment", "Other"]
-    )
-    expense_date = st.date_input("Select a date", value=date(2026, 1, 29))
-    comment = st.text_input("Comment (optional)")
-    submitted = st.form_submit_button(label="Save Expense")
+with tab1:
+    st.title("Expense Tracker")
+    st.write("Register your expenses and get and overview of your spendings")
 
-    if submitted:
-        st.balloons()
-        new_expense = {
-            "amount": amount,
-            "category": category,
-            "date": expense_date,
-            "comment": comment,
-        }
+    st.header("Add a new expense")
+    with st.form(key="expense_form"):
+        amount = st.number_input("Amount (SEK)", min_value=0.0, format="%.2f")
+        category = st.selectbox(
+            "Category", ["Food", "Transport", "Housing", "Entertainment", "Other"]
+        )
+        expense_date = st.date_input("Select a date", value=date(2026, 1, 29))
+        comment = st.text_input("Comment (optional)")
+        submitted = st.form_submit_button(label="Save Expense")
 
-        try:
-            df = pd.read_csv("expenses.csv")
-            df = pd.concat([df, pd.DataFrame([new_expense])], ignore_index=True)
-        except FileNotFoundError:
-            df = pd.DataFrame([new_expense])
+        if submitted:
+            st.balloons()
+            new_expense = {
+                "amount": amount,
+                "category": category,
+                "date": expense_date,
+                "comment": comment,
+            }
 
-        df.to_csv("expenses.csv", index=False)
+            try:
+                df = pd.read_csv("expenses.csv")
+                df = pd.concat([df, pd.DataFrame([new_expense])], ignore_index=True)
+            except FileNotFoundError:
+                df = pd.DataFrame([new_expense])
 
-        st.success("Expense saved!")
-        st.dataframe(df)
+            df.to_csv("expenses.csv", index=False)
+            st.success("Expense Saved! 💸")
+            with tab2:
+                st.dataframe(df)
